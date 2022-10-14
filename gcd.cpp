e5cf2d8ff83caf318b12fbd2aa0db0fe1820e71d
ShivamG365 <115338756+ShivamG365@users.noreply.github.com>
2022-10-15 01:33:02 +0530

    Add files via upload
    This code is used to find out the Highest common factor or better known as GCD or HCF.

---
+++ gcd.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+cin>>a>>b;
+int k;
+for( int i=0;i<1000;i++){
+if (a>=b){
+    int k=a-b;
+    if(b>=k){
+       a=b;
+        b=k;
+    }
+    else if(k>=b){
+        a=k;
+    }
+}
+if(k==0){
+    break;
+}
+}
+cout<<"The HCF of the following Numbers is: "<<a<<endl;
+
+
+    return 0;
+}
+/* HCF = 
+9328,5678=3650
+5678-3650=2028
+3650-2028=1622
+2028-1622=406
+1622-406=1216
+1216-406=810
+810-406=404
+406-404=2
+404-2=402
+402-2=400
+.........
+4-2=2
+2-2=0 ans.
+*/
